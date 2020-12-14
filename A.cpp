b960f51b541dee8d0305bf4c16bc4c0d4c228b44
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-14 21:38:57 +0800

    Update A.cpp

--- A.cpp
+++ A.cpp
@@ -1,12 +1,19 @@
 #include <bits/stdc++.h>
-
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int a, b, c, d;
-  cin >> a >> b >> c >> d;
-  cout << min({a, b, c, d}) << '\n';
-  return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << min({a, b, c, d}) << "\n";
 }
