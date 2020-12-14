2fb4e500c29e0ab51a496f14cc65dd2f41b04b79
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-14 18:18:28 +0800

    Create A.cpp

---
+++ A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  cout << min({a, b, c, d}) << '\n';
+  return 0;
+}
